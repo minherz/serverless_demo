8011014c12f0a8245924122e393b286eb690b816
Leonid Yankulin <leoy@google.com>
2019-09-09 21:54:53 +0300

    fix image-ingester install script
    use placeholder instead of '-input' suffix

--- scripts/install-image-ingester.sh
+++ scripts/install-image-ingester.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 project_id=`gcloud config get-value project`
-sed -i 's/  INPUT_BUCKET_NAME: -input/  INPUT_BUCKET_NAME: '$project_id'-input/g' app.yaml
+sed -i 's/  INPUT_BUCKET_NAME: PLACE_BUCKET_NAME_HERE/  INPUT_BUCKET_NAME: '$project_id'-input/g' app.yaml
 gcloud app deploy --quiet
-sed -i 's/  INPUT_BUCKET_NAME: '$project_id'-input/  INPUT_BUCKET_NAME: -input/g' app.yaml
+sed -i 's/  INPUT_BUCKET_NAME: '$project_id'-input/  INPUT_BUCKET_NAME: PLACE_BUCKET_NAME_HERE/g' app.yaml
